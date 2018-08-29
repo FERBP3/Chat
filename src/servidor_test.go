edbeca6d37a9c0ae72a32cbfe11711e8e56d3544
Fernando Brigido <ferbpp@ciencias.unam.mx>
2018-08-28 22:16:34 -0500

    Se agregaron archivos y pruebas para el chat

---
+++ src/servidor_test.go
@@ -0,0 +1,33 @@
+package src
+
+import "testing"
+
+func TestServidor(t *testing.T){
+	if false{
+		t.Fail()
+	}
+}
+
+func TestGetNombreUsuario(t *testing.T){
+	if false {
+		t.Fail()
+	}
+}
+
+func TestRecibeMensajes(t *testing.T){
+	if false {
+		t.Fail()
+	}
+}
+
+func TestHandleConnection(t *testing.T){
+	if false {
+		t.Fail()
+	}
+}
+
+func TestNumeroClientes(t *testing.T){
+	if false {
+		t.Fail()
+	}
+}
